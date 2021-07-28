3a022f8adae5dde98c999ba055d20bb5b1ccde6a
Denis_Lysogorsky <lysogorskydenis@gmail.com>
2021-07-28 16:09:47 +0300

    connected db

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
 import { UsersService } from './users.service';
 
@@ -7,12 +7,13 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @MessagePattern('getUser')
-  getOrders() {
-    return this.usersService.getUser();
+  getOrders(id: string) {
+    return this.usersService.getUser(id);
   }
 
   @Get()
-  getUser() {
-    return this.usersService.getUser();
+  getUser(@Query() query) {
+    const { id } = query;
+    return this.usersService.getUser(id);
   }
 }
